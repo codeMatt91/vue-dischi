4d98fa91347e9e981aaffd94dbd0b74c8640e939
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-02-10 15:40:48 +0100

    Added files scss + component Header

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
- 
+ <div>
+   <Header />
+   <main></main>
+ </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
 
 export default {
   name: 'App',
   components: {
+    Header,
   }
 }
 </script>
