218fd4565834e9ed150acf57258cc17a98c82401
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-02-10 20:25:11 +0100

    Added img in Header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,15 @@
 <template>
   <header>
     <div class="logo">
-      <i class="fa-brands fa-spotify"></i>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/768px-Spotify_logo_without_text.svg.png"
+        alt="logo spotify"
+      />
     </div>
   </header>
 </template>
 
 <script>
-
 export default {
   name: "Header",
 };
@@ -16,14 +18,20 @@ export default {
 <style lang="scss" scoped>
 @import "../assets/scss/_vars.scss";
 
-
 header {
   background-color: $bg_header;
   padding: 15px 0;
   height: 100px;
   .logo {
+    height: 100%;
     margin-left: 20px;
-    border-radius: 50%;
+    display: flex;
+    align-items: center;
+    img {
+      background-color: $bg_header;
+      width: 50px;
+      height: 50px;
+    }
   }
 }
 </style>
