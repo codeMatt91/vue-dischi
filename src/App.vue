8b2804a4280d6ff1935538d67894847230a9c081
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-02-10 17:32:17 +0100

    Fixed style in MAin + created ajax call

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
- <div>
-   <Header :logo-spotify="logoSpotify"/>
-   <Main />
- </div>
+  <div>
+    <Header />
+    <Main />
+  </div>
 </template>
 
 <script>
@@ -10,18 +10,13 @@ import Header from "./components/Header.vue";
 import Main from "./components/Main.vue";
 
 export default {
-  name: 'App',
-  props: "logoSpotify",
+  name: "App",
+
   components: {
     Header,
     Main,
   },
-  data(){
-    return{
-      logoSpotify: "https://cantabrialabsdifacooper.it/wp-content/uploads/2021/03/png-clipart-spotify-logo-spotify-computer-icons-podcast-music-apps-miscellaneous-angle-300x300.png"
-    }
-  }
-}
+};
 </script>
 
 <style lang="scss">
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,26 +1,18 @@
 <template>
   <main>
     <div class="container">
-      <div id="albums" class="py-5">
-        <div class="row">
-          <div class="offset-1"></div>
-          <div class="col-2">
-            <div class="card" style="width: 18rem">
-              <img src="" class="card-img-top" alt="" />
-              <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-                
-              </div>
-            </div>
+      <div class="albums py-5">
+        <div class="card">
+          <img
+            src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg"
+            class="card-img-top img-fluid"
+            alt="album"
+          />
+          <div class="card-body">
+            <h2 class="card-title">title</h2>
+            <div class="author">Author</div>
+            <div class="year">1968</div>
           </div>
-          <div class="col-2"></div>
-          <div class="col-2"></div>
-          <div class="col-2"></div>
-          <div class="col-2"></div>
         </div>
       </div>
     </div>
@@ -28,8 +20,26 @@
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "Main",
+  data() {
+    return {
+      albums: [],
+    };
+  },
+  methods: {
+    getAlbums() {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/array/music")
+        .then((res) => {
+          this.albums = res.data.response;
+        });
+    },
+  },
+  mounted() {
+    this.getAlbums();
+  },
 };
 </script>
 
@@ -39,5 +49,14 @@ export default {
 main {
   color: white;
   background-color: $bg_main;
+}
+.albums {
+  display: flex;
+  flex-wrap: wrap;
+}
+.card {
+  width: calc(100% / 5);
+  text-align: center;
+  background-color: $bg_card;
 }
 </style>
